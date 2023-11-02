b8f202174c2c6ee4449777a11e20c2c755c83c94
usr-a.esantos <you@example.com>
2023-11-02 17:18:04 +0100

    number type

---
+++ BASES/tipos/number.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    let avengers = 10;
+    console.log(avengers);
+    const villians = 20;
+    if (avengers < villians) {
+        console.log('Estamos en problemas');
+    }
+    else {
+        console.log('Nos salvamos');
+    }
+    // avengers = 123;
+    avengers = Number('55A'); // la función Number() es para pasar un dato a tipo number
+    console.log({ avengers });
+})();
