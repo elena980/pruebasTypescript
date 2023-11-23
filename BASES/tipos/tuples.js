d4547849252452310d39ecdb567d25cda338fffe
usr-a.esantos <you@example.com>
2023-11-23 16:56:17 +0100

    tuples

---
+++ BASES/tipos/tuples.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    // asi de define una tupla, const hero: [string, number] 
+    // para defienrenciarla de un array,  const number: (number|string|boolean) []
+    // en las tuplas los tipos de  datos se definen dentro de las llaves
+    const hero = ['Dr Strange', 100, true];
+    // hero[0] = 50; te lo marca como error porque en la posición 0 siempre tiene que ser de tipo String
+    hero[0] = 'Ironman';
+    hero[1] = 50;
+    hero[2] = false;
+    console.log(hero);
+})();
